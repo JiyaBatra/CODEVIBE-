95fff141c63b480cca2e5cebd10f2d6d84540b77
wuyangfan <1102042793@qq.com>
2026-05-18 01:09:59 +0800

    fix(ui): correct quiz and lesson header copy typos
    Fixes #86

--- client/src/components/Target.jsx
+++ client/src/components/Target.jsx
@@ -6,7 +6,7 @@ const Target = () => {
             <h2>What you will get </h2>
             <ul >
                 <li>simple,beginner-friendly lesson</li>
-                <li>practice quizes to test your knowledge</li>
+                <li>practice quizzes to test your knowledge</li>
                 <li>certificate after course completion</li>
             </ul>
         </section></div>
