9042150635f388ebe3031187b4f4fe692be4f3da
itssagarK <sagarkapri321@gmail.com>
2026-06-02 22:29:43 +0530

    fix: resolve validation mismatch in HtmlLesson2

--- client/src/components/HtmlLesson2.jsx
+++ client/src/components/HtmlLesson2.jsx
@@ -108,7 +108,7 @@ const HtmlLesson2 = () => {
       <Compiler
         hint="💡 Review the lesson instructions carefully. Make sure your output matches exactly." 
         LessonId="html-lesson2"
-        expectedOutput={`<b> bold </b> <i> italic </i> <u> under line </u> <strong> bold important message </strong> <em> italic important message </em> <mark> highlight </mark> <marquee> headline </marquee> <br> <hr> <abbr>HTML</abbr>`}
+        expectedOutput={`<b> bold </b> <i> italic </i> <u> underline </u> <strong> bold important message </strong> <em> italic important message </em> <mark> highlight </mark> <marquee> headline </marquee> <br> <hr> <abbr>HTML</abbr>`}
         initialCode={`<h1> HELLO, From Code Vibe </h1>`}
         onSuccess={handleSuccess}
       />
